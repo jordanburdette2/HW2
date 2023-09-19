9dc8e2c8ad9dd0604048566ceebce4fdb8d2b198
jordanburdette2 <111701847+jordanburdette2@users.noreply.github.com>
2023-09-18 19:12:08 -0500

    Update post-result.php

--- post-result.php
+++ post-result.php
@@ -1,18 +1,17 @@
 <?php
-$pageTitle="Post Result";
+$pageTitle = "Post result";
 include "view-header.php";
 ?>
-    <h1>Post Result</h1>
+<h1>Post result</h1>
 <?php
 echo getDisplay();
 include "view-footer.php";
 
-function getDisplay()
-    {
-        if (isset($_POST['my-name'])) {
-  return "<p>The value sent is: </p>" . $_POST['my_name'];
+function getDisplay() {
+  if (isset($_POST['my-name'])) {
+    return "<p>The value sent is:</p>" . $_POST['my-name'];
 } else {
   return "<p>Nothing posted to the page.</p>";
 }
-    }
+}
 ?>
